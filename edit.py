ad5e4cc6d6e0f3c4dc3f520f55b325e8323226f3
cat40 <19844790+cat40@users.noreply.github.com>
2017-12-09 15:13:41 -0500

    fixed edit

--- edit.py
+++ edit.py
@@ -16,8 +16,10 @@ def edit(filename, key, value):
     songdata = EasyID3(filename)
     value = value.replace('#f', filename)
     for k, v in specials.items():
-        try: value = value.replace(k, songdata[v])
+        try: value = value.replace(k, songdata[v][0])
         except KeyError: pass #if particular file does not have that metadata
     songdata[key] = value
     songdata.save()
         
+if __name__ == '__main__':
+    edit('test files\\2-01 Barbara Allen.mp3', 'title', '222#f')
